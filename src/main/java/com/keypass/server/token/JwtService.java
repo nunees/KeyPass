e73e90facbc5ab6aa395bd863ed3a1073d1de3bb
nunees <felipe_silva_nunes@hotmail.com>
2024-01-30 19:12:38 -0300

    feat: add refresh token generation and docker compose

--- src/main/java/com/keypass/server/token/JwtService.java
+++ src/main/java/com/keypass/server/token/JwtService.java
@@ -3,6 +3,7 @@ package com.keypass.server.token;
 import java.time.Instant;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
@@ -11,24 +12,32 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JwtService {
+public class old_JWTSERVICE {
     private final JwtEncoder encoder;
 
-    public JwtService(JwtEncoder encoder) {
+    @Value("${application.security.jwt.access.token.expiresIn}")
+    private Long accessTokenExpiresIn;
+
+    @Value("${application.security.jwt.refresh.token.expiresIn}")
+    private Long refreshTokenExpiresIn;
+
+    @Value("${application.security.jwt.issuer}")
+    private String tokenIssuer;
+
+    public old_JWTSERVICE(JwtEncoder encoder) {
         this.encoder = encoder;
     }
 
     public String generateToken(Authentication authentication){
         Instant now = Instant.now();
-        long expiresIn = 3600L;
 
         String scopes = authentication.getAuthorities()
                 .stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(" "));
 
         var claims = JwtClaimsSet.builder()
-                .issuer("spring-security-jwt")
+                .issuer(tokenIssuer)
                 .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiresIn))
+                .expiresAt(now.plusSeconds(accessTokenExpiresIn))
                 .subject(authentication.getName())
                 .claim("scope", scopes)
                 .build();
